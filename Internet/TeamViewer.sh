cc229de5ab3faf069591db8400372ea3eee8b167
Yang <354096994@qq.com>
2018-02-14 23:11:50 +0800

    update GPG

--- Internet/TeamViewer.sh
+++ Internet/TeamViewer.sh
@@ -5,6 +5,8 @@ which teamviewer
 if [[ $? -eq 0 ]]; then
 	echo -e "TeamViewer  already installed........\t\033[32;40;1m【√】\033[0m"
 else
+	echo -e "Install the GPG key................................."
+	wget -O - https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc | sudo apt-key add -
 	sudo sh -c 'echo "deb http://linux.teamviewer.com/deb stable main"' | sudo tee /etc/apt/sources.list.d/teamviewer.list
 	sudo apt-get update
 	sudo apt-get install teamviewer -y
