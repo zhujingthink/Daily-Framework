02da4273d042c8d7d529b3d7efc4ff41ce4fc96c
Yang <354096994@qq.com>
2018-02-14 22:09:36 +0800

    update

--- System-Tools/input.sh
+++ System-Tools/input.sh
@@ -10,9 +10,11 @@ function sogouinstall(){
 		sudo cp -r ./x/sogou/keyrings/sogou-archive-keyring.gpg /usr/share/keyrings/
 		sudo cp -r ./x/sogou/keyrings/sogou-archive-keyring.gpg /etc/apt/trusted.gpg.d/
 		sudo sh -c 'echo "deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main"' | sudo tee /etc/apt/sources.list.d/sogoupinyin.list
-		echo 'Update apt sources and install Sublime Text..................................'
+		sudo echo "deb http://mirrors.aliyun.com/debian stretch main" >> /etc/apt/sources.list
+		echo 'Update apt sources and install sogoupinyin..................................'
 		sudo apt-get update
 		sudo apt-get install sogoupinyin -y
+		sudo sed -i 's/deb http:\/\/mirrors.aliyun.com\/debian stretch main/ /' /etc/apt/sources.list
 		echo " "
 		echo -e "Successful installation  \t\033[32;40;1m【√】\033[0m"
 		sleep 2
