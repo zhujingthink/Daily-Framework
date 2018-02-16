8e1b0dd1fa0a499b2848320dd3f01f74cc390cca
Yang <354096994@qq.com>
2018-02-16 22:48:59 +0800

    add gdm adn

--- Gnome-desktop/desktop-art.sh
+++ Gnome-desktop/desktop-art.sh
@@ -15,8 +15,9 @@ function menu(){
 	echo -e "\t\t 【2】   Terminal "
 	echo -e "\t\t 【3】   Gnome3 Themes"
 	echo -e "\t\t 【4】   gnome shell Expand"
-	echo -e "\t\t 【5】   GRUb Themes"
-	echo -e "\t\t 【6】   cairo-dock"
+	echo -e "\t\t 【5】   GDM Themes(务必请先将图片放入~/ 目录并重命名为bg.jpg)"
+	echo -e "\t\t 【6】   GRUb Themes"
+	echo -e "\t\t 【7】   cairo-dock"
 	echo -e "\t\t \033[36;40;1m【0】\033[0m  \033[31;40;1m Return to the main menu\033[0m "
 	read -p "please enter[*]continue：" gnomemenu
 	case $gnomemenu in
@@ -37,11 +38,15 @@ function menu(){
 		menu
 		;;
 		5)
+		./Gnome-desktop/gdm.sh
+		menu
+		;;
+		6)
 		echo -e "[GRUB Themes]:https://www.gnome-look.org/browse/cat/109/"
 		echo -e "[GRUB Themes]:https://www.opendesktop.org/browse/cat/109/ord/latest/"
 		menu
 		;;
-		6)
+		7)
 		which cairo-dock
 		if [[ $? -eq 0 ]]; then
 			echo -e "cairo-dock  already installed........\t\033[32;40;1m【√】\033[0m"
