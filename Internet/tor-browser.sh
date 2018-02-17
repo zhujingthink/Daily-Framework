6fa82d33f291bd22c63622a3544f1cc1b452a666
Yang <354096994@qq.com>
2018-02-18 06:02:56 +0800

    √

--- Internet/tor-browser.sh
+++ Internet/tor-browser.sh
@@ -3,7 +3,7 @@ echo "开始下载....................."
 wget https://github.com/TheTorProject/gettorbrowser/releases/download/v7.5/tor-browser-linux64-7.5_en-US.tar.xz
 echo "正在解压....................."
 tar xvf tor-browser-linux64-7.5_en-US.tar.xz
-
+rm tor-browser-linux64-7.5_en-US.tar.xz
 sudo mv tor-browser_en-US /opt/
 echo "正在创建桌面文件/usr/share/applications/tor-browser.desktop"
 sudo cp -r ./x/tor-browser.desktop /usr/share/applications/
